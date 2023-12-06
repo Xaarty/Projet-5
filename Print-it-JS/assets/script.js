a1ce4cf716d82f5f3a0aad330ae28fb6f4e2d801
Mikael Caillaud <caillaud.mikael.pro@gmail.com>
2023-12-06 18:49:34 +0100

    Mise en place des dots

--- Print-it-JS/assets/script.js
+++ Print-it-JS/assets/script.js
@@ -1,3 +1,31 @@
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	},
+]
+
+let carrouselLength = slides.length
+let Dots = document.querySelector(".dots")
+for (let i = 0; i < slides.length; i++) {
+	let dot = document.createElement("div")
+	dot.classList.add("dot")
+	Dots.appendChild(dot)
+
+}
+console.log(carrouselLength)
 
 //Event listener vérification du "clic" gauche à la souris
 
